82e09878b45bef38ff05589eb2d7c1a9903a5a60
Chandur Dissanayake <chandurdissanayake0@gmail.com>
2021-09-25 23:12:00 +0530

    list-view

--- app/src/main/java/exportkit/xd/welcome_coffee_activity.java
+++ app/src/main/java/exportkit/xd/welcome_coffee_activity.java
@@ -2,18 +2,26 @@
 package exportkit.xd;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 
 public class welcome_coffee_activity extends Activity {
 
 	// Create button object
+	public ListView listView;
 	private View _bg__welcome_coffee_ek2;
 	private View expresso_bakcground;
 	private View circle14;
@@ -69,7 +77,7 @@ public class welcome_coffee_activity extends Activity {
 	// Arrays for items in the shop
 	String title[] = {"Coffee Black","Expresso Coffee","Light Coffee","Cappuccino","Coffee Latte"};
 	String description[] = {"Rs/- 2500","Rs/- 5000","Rs/-4500","Rs/-9000","Rs/-9999"};
-	int image[] = {R.drawable.Black,R.drawable.Expresso,R.drawable.Light,R.drawable.Cappochunio,R.drawable.Latte};
+	int image[] = {R.drawable.coffee,R.drawable.coffee,R.drawable.coffee,R.drawable.coffee,R.drawable.coffee};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -133,7 +141,49 @@ public class welcome_coffee_activity extends Activity {
 			}
 		});
 
+		// Set adapter
+		listView = findViewById(R.id.listView);
+		CustomeAdapter adapter = new CustomeAdapter(this, title, image, description);
+		listView.setAdapter(adapter);
 	}
 }
-	
+
+class CustomeAdapter extends ArrayAdapter<String>{
+
+	// creating objects of adapter class
+	Context context;
+	int [] images;
+	String [] title;
+	String [] des;
+
+	CustomeAdapter(Context context, String[] title, int[] images, String[] des){
+		super(context, R.layout.list_view, R.id.title, title);
+		this.context = context;
+		this.images = images;
+		this.title = title;
+		this.des = des;
+	}
+
+	@NonNull
+	@Override
+	public View getView(int position, @NonNull View convertView, @NonNull ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		// converting list_view.xml to a java object
+		View row = inflater.inflate(R.layout.list_view, parent,false);
+
+		// creating ListView in activity and get reference to that ListView
+		ImageView imageView = row.findViewById(R.id.imageView);
+		TextView titleView = row.findViewById(R.id.title);
+		TextView descriptionView = row.findViewById(R.id.description);
+
+		// Set images dynamically
+		imageView.setImageResource(images[position]);
+		titleView.setText(title[position]);
+		descriptionView.setText(des[position]);
+
+		return row;
+	}
+}
+
 	
