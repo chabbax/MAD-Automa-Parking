31032ca6c39739c079435db41de44fa5db1f1a9d
Chandur Dissanayake <chandurdissanayake0@gmail.com>
2021-09-26 20:28:27 +0530

    finalized-crud-design

--- app/src/main/java/exportkit/xd/update.java
+++ app/src/main/java/exportkit/xd/update.java
@@ -3,12 +3,22 @@ package exportkit.xd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class update extends AppCompatActivity {
+
+    // Creating objects
+    private EditText title, desc;
+    private View edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        title = findViewById(R.id.edit_type_name);
+        desc = findViewById(R.id.edit_type_cardno);
+        edit = findViewById(R.id.edit_total_amount);
     }
 }
